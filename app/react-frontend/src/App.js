0f2fd4cb9cc34fc8c1c64bb221101e50b981a988
אלכס <romjacoby@gmail.com>
2020-12-02 15:25:06 +0200

    ya

--- app/react-frontend/src/App.js
+++ app/react-frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import NavigationBar from './Components/NavigationBar'
